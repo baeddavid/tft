783f45b6f91460957df18baa9410a9cd88b75b24
David Bee <baeddavid@gmail.com>
2019-12-06 13:37:10 -0600

    add temporary login page

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import userService from "../../utils/userService";
+
+class LoginPage extends  Component {
+    state = {
+        email: "",
+        pasword: ""
+    };
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        try {
+            await userService.login(this.state);
+            this.props.handleSignupOrLogin();
+            this.props.history.push("/");
+        } catch (err) {
+            alert("INVALID CREDENTIALS");
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <header>Log In</header>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <div>
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={this.state.email}
+                                name="email"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                value={this.state.pw}
+                                name="pw"
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <button>Log in</button>
+                            <Link to="/">Cancel</Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
