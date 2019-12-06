5aceb19cdebdff72dd04679ce8f8cb83080357ca
David Bee <baeddavid@gmail.com>
2019-12-06 13:32:06 -0600

    set up main App component to host components

---
+++ src/pages/AppPage/App.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import userService from "../../utils/userService";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: userService.getUser();
+        }
+    };
+
+    handleLogout = () => {
+        userService.logout();
+        this.setState({ user: null });
+    }
+
+    handleSignupOrLogin = () => {
+        this.setState({ user: userService.getUser() });
+    }
+
+    render() {
+        return(
+            <div>
+                <Switch>
+                    <Route
+                        exact
+                        path="/login"
+                        render={({ history }) => (
+                            <LoginPage
+                                history={history}
+                                handleSignupOrLogin={this.handleSignupOrLogin()}
+                                />
+                        )}
+                        />
+                    <Route
+                        exact
+                        path="/signup"
+                        render={({ history }) => (
+                            <SignupPage
+                                history={history}
+                                handleSignupOrLogin={this.handleSignupOrLogin}
+                            />
+                        )}
+                    />
+                </Switch>
+            </div>
+        )
+    }
+}
