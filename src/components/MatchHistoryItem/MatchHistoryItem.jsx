13f14b62e2157d30b291987fa90939ddd55d9b8f
David Bee <baeddavid@gmail.com>
2019-12-10 17:08:15 -0600

    matchHistoryItem displays levels and little legends

--- src/components/MatchHistoryItem/MatchHistoryItem.jsx
+++ src/components/MatchHistoryItem/MatchHistoryItem.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card, ListGroup } from "react-bootstrap"
 import * as RiotApi from "../../services/riot-api";
+import styles from "./MatchHistoryItem.module.css";
 
 class MatchHistoryItem extends Component {
     constructor(props) {
@@ -12,7 +13,7 @@ class MatchHistoryItem extends Component {
     }
 
     componentDidMount() {
-        RiotApi.getPuuidFromSummonerName("metalkarp")
+        RiotApi.getPuuidFromSummonerName(this.props.summoner)
             .then(res => this.setState({ puuid: res }))
     }
 
@@ -47,18 +48,41 @@ class MatchHistoryItem extends Component {
         return roundedTime;
     }
 
+    getLittleLegendsLevel() {
+        let level;
+        for(let player of this.props.match.participants) {
+            if(player.puuid === this.state.puuid) {
+                level = player.level;
+            }
+        }
+        return level;
+    }
+
+    getLittleLegends() {
+        let companion;
+        for(let player of this.props.match.participants) {
+            if(player.puuid === this.state.puuid) {
+                companion = player.companion.species;
+            }
+        }
+        return companion;
+    }
+
     render() {
         let placement = this.summonerPlacement();
         let placementStringObject = this.placementToString(placement);
         let matchDuration = this.getMatchDuration();
+        let playerLevel = this.getLittleLegendsLevel();
+        let littleLegends = this.getLittleLegends();
 
         return(
-            <Card style={{ width: "35rem"}}>
+            <Card className={styles.matchCard}>
                 <Card.Header>{ placementStringObject[placement] }</Card.Header>
-                <ListGroup variant="flush">
-                    <ListGroup.Item>{ this.props.summoner }</ListGroup.Item>
-                    <ListGroup.Item>{ matchDuration }</ListGroup.Item>
-                </ListGroup>
+                <Card.Body>
+                    <div>{ littleLegends }</div>
+                    <div>Level { playerLevel }</div>
+                </Card.Body>
+                <footer className="blockquote-footer">{ this.props.summoner } { matchDuration }</footer>
             </Card>
         )
     }
--- src/pages/AppPage/App.js
+++ src/pages/AppPage/App.js
@@ -67,7 +67,7 @@ class App extends Component {
                     />
                     <Route
                         exact
-                        path="/"
+                        path="/matches"
                         render={({ history }) => (
                             <MatchHistoryList
                                 matchHistory={this.state.matchHistory}
